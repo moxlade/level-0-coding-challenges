e10c7e8a0c43613bace9e78552f7fa85cc824236
Andile <motheox@gmail.com>
2021-10-20 21:06:43 +0200

    Updated Task 0.6 bonus

--- zero-six-bonus.py
+++ zero-six-bonus.py
@@ -1,17 +1,17 @@
 def maxi_number(*args):
-    max = 0
+    maximum = 0
     index = 0
     for i in args:
         index += 1
     for j in range(0,index):
-        if args[j] >= max:
-            max = args[j]    
+        if args[j] >= maximum:
+            maximum = args[j]    
 
-    return max
+    return maximum
 
              
         
-    
+
         
 
 
