e68ec7c1404ed5de33eebe9169d559529d2addf0
Andile <motheox@gmail.com>
2021-10-21 17:50:50 +0200

    Task 0.10

---
+++ zero-ten.py
@@ -0,0 +1,12 @@
+def common_letters(s1,s2):
+    common = ""
+    for i in range(len(s2)):
+        if s2[i] in s1:
+            common = common + s2[i] + ", "
+    if not common:
+        return "No common letters"    
+
+    common = common[0:-2]
+    return "\'Common letters: " + common + "\'"
+
+
