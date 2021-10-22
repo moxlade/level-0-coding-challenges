7c63d7ef4f62956bb56d744e19c5a4e4d0e4ecbb
Andile <motheox@gmail.com>
2021-10-22 16:32:20 +0200

    Reviewed Task 0.8

--- zero-eight.py
+++ zero-eight.py
@@ -2,18 +2,17 @@ def time_converter(x):
     hours = x//60
     minutes = (x % 60)
     total_time = " "
-    if hours >= 2:
+    if hours >= 2 or hours == 0:
         total_time = str(hours) + " hours, "
     elif hours == 1:
         total_time = str(hours) + " hour, "
     if minutes == 1:
         total_time = total_time + str(minutes) + " minute "
-    elif minutes >= 2:
+    elif minutes >= 2 or minutes == 0:
         total_time = total_time + str(minutes) + " minutes "
-    if minutes == 0:
-        total_time = total_time[0:-2]
-    elif hours == 0:
-        total_time = total_time[1:]
+    
+    total_time = total_time[0:-1]
     return total_time
+
 
 
